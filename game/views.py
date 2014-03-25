5b2450d4c62aaf230078760444df10a30f5d0716
CBedo <carlos.bedoy@gmail.com>
2014-03-25 17:17:37 -0600

    Se define view con su respectivo template

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_responde
 
 # Create your views here.
+
+def home(request):
+    return render_to_responde('index.html')
