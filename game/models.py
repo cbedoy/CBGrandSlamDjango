4f3c1ce1899ce4b2e8c28ba8e61871a86049851d
CBedoy <carlos.bedoy@gmail.com>
2014-03-25 00:05:52 -0600

    Reindentando

--- game/models.py
+++ game/models.py
@@ -3,34 +3,34 @@ from django.db import models
 # Create your models here.
 
 class Country(models.Model):
-    name        = models.CharField(max_length=45)
+    name = models.CharField(max_length=45)
 
     def __unicode__(self):
         return self.name
 
 class Location(models.Model):
-    name        = models.CharField(max_length=45)
-    longitud    = models.FloatField()
-    latitud     = models.FloatField()
-    country     = models.ForeignKey(Country)
+    name = models.CharField(max_length=45)
+    longitud = models.FloatField()
+    latitud = models.FloatField()
+    country = models.ForeignKey(Country)
 
     def __unicode__(self):
         return self.name
 
 class Tournament(models.Model):
-    name    =   models.CharField(max_length=45)
-    year    =   models.DateField('2007')
-    name    =   models.TextField(max_length=300)
-    country =   models.ForeignKey(Country)
+    name = models.CharField(max_length=45)
+    year = models.DateField('2007')
+    name = models.TextField(max_length=300)
+    country = models.ForeignKey(Country)
 
     def __unicode__(self):
         return self.name
 
 
 class Referee(models.Model):
-    firstName   =   models.CharField(max_length=45)
-    lastName    =   models.CharField(max_length=45)
-    time        =   models.CharField(max_length=45)
+    firstName = models.CharField(max_length=45)
+    lastName = models.CharField(max_length=45)
+    time = models.CharField(max_length=45)
 
     def __unicode__(self):
         return self.firstName+self.lastName
@@ -48,7 +48,7 @@ class Game(models.Model):
     modality = models.CharField(max_length=45)
 
     def __unicode__(self):
-        return  self.modality
+        return self.modality
 
 class Player (models.Model):
     firstName = models.CharField(max_length=45)
