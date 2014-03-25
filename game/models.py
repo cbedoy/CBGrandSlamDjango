73020f7cc17ef1d40ba0afd7415b3df87830af01
CBedoy <carlos.bedoy@gmail.com>
2014-03-24 23:41:18 -0600

    Añadiendo campos

--- game/models.py
+++ game/models.py
@@ -3,22 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Country(models.Model):
-    name    = models.CharField(max_length=45)
+    name        = models.CharField(max_length=45)
 
     def __unicode__(self):
         return self.name
 
 class Location(models.Model):
-    name    = models.CharField(max_length=45)
-    country = models.ForeignKey(Country)
+    name        = models.CharField(max_length=45)
+    longitud    = models.FloatField()
+    latitud     = models.FloatField()
+    country     = models.ForeignKey(Country)
 
     def __unicode__(self):
         return self.name
 
 class Tournament(models.Model):
-    name    = models.CharField(max_length=45)
-    year    = models.DateField('2007')
-    country = models.ForeignKey(Country)
+    name    =   models.CharField(max_length=45)
+    year    =   models.DateField('2007')
+    name    =   models.TextField(max_length=300)
+    country =   models.ForeignKey(Country)
 
     def __unicode__(self):
         return self.name
