318342cb67a892e0d26728668a70e31f5b2e3539
CBedoy <carlos.bedoy@gmail.com>
2014-03-25 00:25:30 -0600

    Registrando modelos en admin

--- game/admin.py
+++ game/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from game.models import Location, Country, Tournament
+from game.models import *
 # Register your models here.
 
 admin.site.register(Location)
 admin.site.register(Country)
 admin.site.register(Tournament)
+admin.site.register(Nationality)
+admin.site.register(Category)
+admin.site.register(Player)
+admin.site.register(Referee)
+admin.site.register(Trainer)
+admin.site.register(Modality)
+admin.site.register(Game)
