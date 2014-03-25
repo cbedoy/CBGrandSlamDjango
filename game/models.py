f1e48c196fbb25bf2208dc3afe44520c948f7e5d
CBedoy <carlos.bedoy@gmail.com>
2014-03-24 23:37:41 -0600

    Renombramiento de los modelos

--- game/models.py
+++ game/models.py
@@ -2,14 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-class pais(models.Model):
-    nombre = models.CharField(max_length=45)
+class Country(models.Model):
+    name    = models.CharField(max_length=45)
 
-class lugar(models.Model):
-    nombre = models.CharField(max_length=45)
-    idPais = models.ForeignKey(pais)
+    def __unicode__(self):
+        return self.name
 
-class torneo(models.Model):
-    nombre = models.CharField(max_length=45)
-    ano    = models.DateField('2007')
-    idPais = models.ForeignKey(pais)
+class Location(models.Model):
+    name    = models.CharField(max_length=45)
+    country = models.ForeignKey(Country)
+
+    def __unicode__(self):
+        return self.name
+
+class Tournament(models.Model):
+    name    = models.CharField(max_length=45)
+    year    = models.DateField('2007')
+    country = models.ForeignKey(Country)
+
+    def __unicode__(self):
+        return self.name
