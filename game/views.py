dacddeb379d147a45820a1babccc46e1183e3a74
CBedo <carlos.bedoy@gmail.com>
2014-03-25 22:32:41 -0600

    Cambios en view

--- game/views.py
+++ game/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render_to_response
 
-from django.shortcuts import render_to_response
-
 from game.models import Location
 
-def home(request):
-    entradas = Location.objects.all()
-    return render_to_response('index.html', {'articulos' : entradas})
+def Locations(request):
+    data = Location.objects.all()
+    return render_to_response('index.html', {'articulos' : data})
