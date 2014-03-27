65813f7bca23e419642c23ec4fb5a326444f36e6
CBedoy <carlos.bedoy@gmail.com>
2014-03-26 23:07:26 -0600

    Follow indications django

--- game/views.py
+++ game/views.py
@@ -2,47 +2,58 @@ from django.shortcuts import render_to_response
 
 from .models import *
 
+
 def awards(request):
     data = Award.objects.all()
     return render_to_response('locations.html', dict(awards=data))
+
 
 def categories(request):
     data = Category.objects.all()
     return render_to_response('locations.html', dict(categories=data))
 
+
 def countries(request):
     data = Country.objects.all()
     return render_to_response('locations.html', dict(countries=data))
+
 
 def games(request):
     data = Game.objects.all()
     return render_to_response('locations.html', dict(games=data))
 
+
 def locations(request):
     data = Location.objects.all()
     return render_to_response('locations.html', dict(locations=data))
+
 
 def modalities(request):
     data = Modality.objects.all()
     return render_to_response('locations.html', dict(modalities=data))
 
+
 def nationalities(request):
     data = Nationality.objects.all()
     return render_to_response('locations.html', dict(nationalities=data))
+
 
 def players(request):
     data = Player.objects.all()
     return render_to_response('locations.html', dict(players=data))
 
+
 def referees(request):
     data = Referee.objects.all()
     return render_to_response('locations.html', dict(referees=data))
+
 
 def teams(request):
     doubleTeams = DoubleTeam.objects.all()
     singleTeams = SingleTeam.objects.all()
     data = [doubleTeams, singleTeams]
     return render_to_response('locations.html', dict(teams=data))
+
 
 def trainers(request):
     data = Trainer.objects.all()
