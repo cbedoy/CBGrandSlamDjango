ed455956723dfdb69dfaf61eb150247507d71c9c
CBedoy <carlos.bedoy@gmail.com>
2014-03-26 23:04:10 -0600

    Define single views with all data

--- game/views.py
+++ game/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render_to_response
 
-from game.models import Location
+from .models import *
 
-def allLocations(request):
+def getAwards(request):
+    data = Award.objects.all()
+    return render_to_response('locations.html', dict(awards=data))
+
+def getCategories(request):
+    data = Category.objects.all()
+    return render_to_response('locations.html', dict(categories=data))
+
+def getCountries(request):
+    data = Country.objects.all()
+    return render_to_response('locations.html', dict(countries=data))
+
+def getGames(request):
+    data = Game.objects.all()
+    return render_to_response('locations.html', dict(games=data))
+
+def getLocations(request):
     data = Location.objects.all()
-    return render_to_response('locations.html', {'articulos' : data})
+    return render_to_response('locations.html', dict(locations=data))
+
+def getModalities(request):
+    data = Modality.objects.all()
+    return render_to_response('locations.html', dict(modalities=data))
+
+def getNationalities(request):
+    data = Nationality.objects.all()
+    return render_to_response('locations.html', dict(nationalities=data))
+
+def getPlayers(request):
+    data = Player.objects.all()
+    return render_to_response('locations.html', dict(players=data))
+
+def getReferees(request):
+    data = Referee.objects.all()
+    return render_to_response('locations.html', dict(referees=data))
+
+def getTeams(request):
+    doubleTeams = DoubleTeam.objects.all()
+    singleTeams = SingleTeam.objects.all()
+    data = [doubleTeams, singleTeams]
+    return render_to_response('locations.html', dict(teams=data))
+
+def getTrainers(request):
+    data = Trainer.objects.all()
+    return render_to_response('locations.html', dict(trainers=data))
+
