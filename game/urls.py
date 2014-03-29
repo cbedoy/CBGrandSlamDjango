af0517283688cde9f828e229883b7d92bc14094d
CBedo <carlos.bedoy@gmail.com>
2014-03-28 19:32:13 -0600

    Define urls

--- game/urls.py
+++ game/urls.py
@@ -1,1 +1,25 @@
 __author__ = 'administrador'
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+
+    url(r'^Award/', 'game.views.addAward'),
+    url(r'^Category/', 'game.views.addCategory'),
+    url(r'^Country/', 'game.views.addCountry'),
+    url(r'^Game/', 'game.views.addGame'),
+    url(r'^Location/', 'game.views.addLocation'),
+    url(r'^Modality/', 'game.views.addModality'),
+    url(r'^Nationality/', 'game.views.addNationality'),
+    url(r'^Player/', 'game.views.addPlayer'),
+    url(r'^Referee/', 'game.views.addReferee'),
+    url(r'^Team/', 'game.views.addTeam'),
+    url(r'^Trainer/', 'game.views.addTrainer'),
+
+
+
+)
