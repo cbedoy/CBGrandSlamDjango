743855f21de131c9aa9d85c20bce2df6bb49f79a
CBedoy <carlos.bedoy@gmail.com>
2014-03-30 12:48:11 -0600

    Delete deprecated view

--- game/views.py
+++ game/views.py
@@ -88,18 +88,6 @@ def newAward(request):
     return render_to_response('new_item.html', {'form': form, "name":name, "app":proj}, context_instance=RequestContext(request))
 
 
-def newCategory(request):
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/')
-    else:
-        form = CategoryForm()
-    name = "New category"
-    proj = "CBGranSlam"
-    return render_to_response('new_item.html', {'form': form, "name":name, "app":proj}, context_instance=RequestContext(request))
-
 
 def newCountry(request):
     if request.method == 'POST':
